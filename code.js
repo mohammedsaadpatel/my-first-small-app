ff62de7458e97e7b3413a813522076e6786c8068
Mohammed Saad <74590450+mohammedsaadpatel@users.noreply.github.com>
2021-01-14 14:50:15 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,71 @@
+onEvent("button1", "click", function( ) {
+  setScreen("geniescreen");
+});
+
+onEvent("back1", "click", function( ) {
+  setScreen("Home_Screen");
+});
+
+onEvent("button2", "click", function( ) {
+  setScreen("dhoni_screen");
+});
+
+onEvent("back2", "click", function( ) {
+  setScreen("Home_Screen");
+});
+
+onEvent("button3", "click", function( ) {
+  setScreen("cristanio_screen");
+});
+
+onEvent("back3", "click", function( ) {
+  setScreen("Home_Screen");
+});
+
+onEvent("button4", "click", function( ) {
+  setScreen( "elon_screen");
+});
+
+onEvent("back4", "click", function( ) {
+  setScreen("Home_Screen");
+});
+
+onEvent("button5", "click", function( ) {
+  setScreen("billgates_screen");
+});
+
+onEvent("back5", "click", function( ) {
+  setScreen("Home_Screen");
+});
+
+onEvent("button6", "click", function( ) {
+  setScreen( "SRK_screen");
+});
+
+onEvent("back6", "click", function( ) {
+  setScreen("Home_Screen");
+});
+
+onEvent("button7", "click", function( ) {
+  setScreen("therock_screen");
+});
+
+onEvent("back7", "click", function( ) {
+  setScreen("Home_Screen");
+});
+
+onEvent("button8", "click", function( ) {
+  setScreen( "jeffbezos_screen");
+});
+
+onEvent("back8", "click", function( ) {
+  setScreen("Home_Screen");
+});
+
+onEvent("button9", "click", function( ) {
+  setScreen( "alberteinstein_screen");
+});
+
+onEvent("back9", "click", function( ) {
+  setScreen("Home_Screen");
+});
